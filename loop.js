213cadaf61bfecc706e5c9b3ebee6fae94b64b6a
vishu pyasi <vpyasi11@gmail.com>
2023-01-08 23:38:37 +0530

    7 Jan: Loops and strings

---
+++ loop.js
@@ -0,0 +1,23 @@
+for( var i=1;i<=10;i++){
+    console.log(i);
+}
+
+var i=0;
+while(i<=10){
+    console.log(i);
+    i+=2;
+}
+
+//Print 5 multiples of a number
+let a=prompt(parseInt("Enter Any Number: "))
+
+let i=1;
+while(i<=5){
+    a=a*i;
+    console.log(a);
+    i++;
+}
+
+for(i=1;i<=5;i++){
+    console.log(a*i);
+}
