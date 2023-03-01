1ce11abe46f33ef14fef997d50e8f1ae83c84711
vishu pyasi <vpyasi11@gmail.com>
2023-03-01 18:00:47 +0530

    Array: operators

---
+++ operators.js
@@ -0,0 +1,53 @@
+//spread and rest operator
+
+const testArr = [1,2,3]
+const testArr2 = [...testArr, 5, 6]
+
+const add(a,b,c){
+    return a+b+c
+}
+
+console.log(add(...testArr))
+
+
+function swap(items, leftIndex, rightIndex){
+    var temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
+}
+function partition(items, left, right) {
+    var pivot   = items[Math.floor((right + left) / 2)], //middle element
+        i       = left, //left pointer
+        j       = right; //right pointer
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j); //sawpping two elements
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+function quickSort(items, left, right) {
+    var index;
+    if (items.length > 1) {
+        index = partition(items, left, right); 
+        if (left < index - 1) { 
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) { 
+            quickSort(items, index, right);
+        }
+    }
+    return items;
+}
+var items = [15,22,7,4,12,9];
+var sortedArray = quickSort(items, 0, items.length - 1);
+console.log(sortedArray);
